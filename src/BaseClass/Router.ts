37a92a0a157ed941bad273f29281aac4d7782f7d
Jundi Alwan <jundi.alwan@gmail.com>
2018-10-12 20:24:32 +0700

    Fixing core concept Router base class

--- src/BaseClass/Router.ts
+++ src/BaseClass/Router.ts
@@ -1,3 +1,16 @@
-class Router {}
+import * as express from 'express'
+
+class Router {
+  private basePath: string
+  private router: Router[]
+
+  constructor(basePath: string) {
+    this.basePath = basePath
+  }
+
+  public registerRouter(router: Router) {
+    this.router.push(router)
+  }
+}
 
 export default Router
